c42b5ee69d83c9b1cfecedae94398cfd75f03f36
deeshavon <deja.greene@tlgcohort.com>
2022-09-08 12:07:07 -0700

    Enum inputs

--- src/main/java/com/tlglearning/FizzBuzz.java
+++ src/main/java/com/tlglearning/FizzBuzz.java
@@ -1,21 +1,20 @@
 package com.tlglearning;
 
-public class FizzBuzz {
+import java.util.EnumSet;
+import java.util.Set;
 
-  public static final String FIZZ_RESULT = "fizz"; //this is a constant
-  public static final String BUZZ_RESULT = "buzz"; //this is a constant
+public enum FizzBuzz {
 
-  public static String valueOf(int num) { //the return type is String
-    String result = "";
+  FIZZ, BUZZ;
+
+  public static Set<FizzBuzz> valueOf(int num) { //the return type is String
+    Set<FizzBuzz> result = EnumSet.noneOf(FizzBuzz.class);
     if (num % 3 == 0) {//IF is always followed by parentheses
-      result = FIZZ_RESULT; //+= is the same as inputting -->... result = result +
+      result.add(FIZZ); //+= is the same as inputting -->... result = result +
     }
     if (num % 5 == 0) {
-      result += BUZZ_RESULT;
+      result.add(BUZZ);
     } // result is an instance of the String class
-    if (result.isEmpty()) {//invoking isEmpty
-      result = String.valueOf(num); //this is a static method of the String class
-    }
     return result;
   }
 
--- src/main/java/com/tlglearning/Main.java
+++ src/main/java/com/tlglearning/Main.java
@@ -1,12 +1,19 @@
 package com.tlglearning;
 
+import java.util.Set;
+
 public class Main {
 
   public static void main(String[] args) { //this is a declaration method, the psvm inputs. I'm going to invoke...in braces{} this is implementation
-    for (int i = 1; i <= 100; i++) {   //using a block compound statement in the braces {}
-      //line 6 above is a "for loop" line of code
-      System.out.println(FizzBuzz.valueOf(i));
-    }
+    int i = 1;
+    do {   //using a block compound statement in the braces {} //line 6 above is a "for loop" line of code
+      Set<FizzBuzz> value = FizzBuzz.valueOf(i);
+      System.out.printf("%d: %s%n", i, value);
+      if (value.equals("fizzbuzz")) {
+        break;
+      }
+      i++;
+    } while (i <= 100);
   }
 
 }
